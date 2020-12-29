61b488599efba46e37f5c81ac270635237c272fc
Joseph Headley <jnh3@williams.edu>
2020-12-29 03:54:12 -0500

    Added More Anime Web Scraping Info

--- Python_Folder/Web_Scraping_Folder/animescrape.py
+++ Python_Folder/Web_Scraping_Folder/animescrape.py
@@ -10,19 +10,30 @@ soup = bs(source, 'lxml')
 csv_file = open("animescrape.csv", "w")
 
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(["Title", "Score", "Medium", "Number of Episodes", "Start Date", "End Date", "Number of Members", "URLS", "Synopses", "Genres"])
+csv_writer.writerow(["Title", "Score", "Medium", "Number of Episodes", "Episode Length", "Start Date", "End Date", "Premier Season", "Source Material", "Age Rating", "Number of Members", "URLS", "Synopses", "Genre 1", "Genre 2", "Genre 3", "Genre 4", "Genre 5", "Genre 6", "Genre 7", "Genre 8"])
 
 names = []
 scores = []
 media = []
 ep_nums = []
-start_dates =[]
+ep_lens = []
+start_dates = []
 end_dates = []
+seasons = []
+materials = []
+age_ratings = []
 mem_nums = []
 urls = []
 synopses = []
 gnrs = []
-genres = []
+genre_1 = []
+genre_2 = []
+genre_3 = []
+genre_4 = []
+genre_5 = []
+genre_6 = []
+genre_7 = []
+genre_8 = []
 
 # print(soup.prettify())
 
@@ -74,7 +85,12 @@ for match in soup.find_all('div', class_='information di-ib mt4'):
     info = [i.strip() for i in info]
 
     medium = re.search(r'\w+', info[1]).group(0)
-    ep_num = re.search(r'\d+', info[1]).group(0)
+
+    try:
+        ep_num = re.search(r'\d+', info[1]).group(0)
+    except Exception as e:
+        ep_num = None
+
     start_date = info[2].split(" - ")[0]
 
     try:
@@ -97,29 +113,141 @@ def remove_all(listobj):
     for val in temp:
         if str(val) == '':
             listobj.remove(val)
-    for j in range(4): #3 of the summaries get cut off because of this for loop
-        listobj.pop() #Consider changing this to check regex instead
+    if "(Source:" in listobj:
+        listobj.pop()
+        listobj.pop()
+    elif "[Written" in listobj:
+        listobj.pop()
+        listobj.pop()
+        listobj.pop()
+        listobj.pop()
     return listobj
 
+""" def convtomins(time):
+    mins = 0
+    if time.group(2) == 'hr':
+        mins += int(time.group(1))*60 
+    if time.group(4) == 'min':
+        mins += int(time.group(3))
+    return mins """
+
+def add_genres(gnrs):
+    genre_1.append(gnrs[0])
+
+    if len(gnrs) == 2:
+        genre_2.append(gnrs[1])
+        genre_3.append(None)
+        genre_4.append(None)
+        genre_5.append(None)
+        genre_6.append(None)
+        genre_7.append(None)
+        genre_8.append(None)
+
+    if len(gnrs) == 3:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(None)
+        genre_5.append(None)
+        genre_6.append(None)
+        genre_7.append(None)
+        genre_8.append(None)
+    
+    if len(gnrs) == 4:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(gnrs[3])
+        genre_5.append(None)
+        genre_6.append(None)
+        genre_7.append(None)
+        genre_8.append(None)
+
+    if len(gnrs) == 5:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(gnrs[3])
+        genre_5.append(gnrs[4])
+        genre_6.append(None)
+        genre_7.append(None)
+        genre_8.append(None)
+
+    if len(gnrs) == 6:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(gnrs[3])
+        genre_5.append(gnrs[4])
+        genre_6.append(gnrs[5])
+        genre_7.append(None)
+        genre_8.append(None)
+
+    if len(gnrs) == 7:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(gnrs[3])
+        genre_5.append(gnrs[4])
+        genre_6.append(gnrs[5])
+        genre_7.append(gnrs[6])
+        genre_8.append(None)
+
+    if len(gnrs) == 8:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(gnrs[3])
+        genre_5.append(gnrs[4])
+        genre_6.append(gnrs[5])
+        genre_7.append(gnrs[6])
+        genre_8.append(gnrs[7])
+
 for url in urls:
-    newsource = requests.get(url).text
-    newsoup = bs(newsource, 'lxml')
+    newsoup = bs(requests.get(url).text, 'lxml')
 
-    match1 = newsoup.find('div', class_='js-scrollfix-bottom-rel')
-    summary = str(match1.table.p.text)
+    summary = str(newsoup.find('div', class_='js-scrollfix-bottom-rel').table.p.text)
     synopsis = " ".join(remove_all(re.split(r'\s',summary)))
-
     synopses.append(synopsis)
 
-    for gnr in newsoup.find_all('span', itemprop = "genre"):
-        genre = gnr.text
-        gnrs.append(genre)
+    studio = newsoup.find('span', class_='dark_text')
 
-    gnrs_cp = gnrs[:]    
-    genres.append(gnrs_cp)
+    for genre in newsoup.find_all('span', itemprop = "genre"):
+        gnrs.append(genre.text)
+
+    add_genres(gnrs)
     gnrs.clear()
 
+    foundseason = False
+    foundeplen = False
+    foundagerat = False
+    foundmaterial = False
+    for item in newsoup.find_all('td', class_ = 'borderClass'):
+        season = re.search(r"(Spring|Summer|Fall|Winter).\d{4}", item.text)
+        ep_len = re.search(r"(\d{1,2})\s(min|hr)\.\s?([a-z0-9]+)?\s?(\w+)?\.?", item.text)
+        age_rating = re.search(r"(R|PG)\s?[0-9+-]+\s?([0-9+]+)?", item.text)
+        material = re.search(r"(Manga|Visual novel|Game|Original|Novel|Light novel|Web manga|Other)\s{2}", item.text)
+
+        if season != None:
+            seasons.append(season.group(1))
+            foundseason = True
+
+        if ep_len != None:
+            ep_lens.append(ep_len.group(0))
+            foundeplen = True
+            
+        if age_rating != None:
+            age_ratings.append(age_rating.group(0))
+            foundagerat = True
+
+        if material != None:
+            materials.append(material.group(1))
+            foundmaterial = True
+
+    if foundseason == False:
+        seasons.append(None)
+    if foundeplen == False:
+        ep_lens.append(None)
+    if foundagerat == False:
+        age_ratings.append(None)
+    if foundmaterial == False:
+        materials.append(None)
+
 for i in range(len(names)):
-    csv_writer.writerow([names[i], scores[i], media[i], ep_nums[i], start_dates[i], end_dates[i], mem_nums[i], urls[i], synopses[i], genres[i]])
+    csv_writer.writerow([names[i], scores[i], media[i], ep_nums[i], ep_lens[i], start_dates[i], end_dates[i], seasons[i], materials[i], age_ratings[i], mem_nums[i], urls[i], synopses[i], genre_1[i], genre_2[i], genre_3[i], genre_4[i], genre_5[i], genre_6[i], genre_7[i], genre_8[i]])
 
 csv_file.close()
--- Python_Folder/Web_Scraping_Folder/scrape_test.py
+++ Python_Folder/Web_Scraping_Folder/scrape_test.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup as bs
+# from lxml import html
 import requests
 import csv 
 import re 
 
-source = requests.get("https://myanimelist.net/anime/33050/Fate_stay_night_Movie__Heavens_Feel_-_III_Spring_Song").text
+source = requests.get("https://myanimelist.net/anime/37491/Gintama__Shirogane_no_Tamashii-hen_-_Kouhan-sen").text
 
 soup = bs(source, 'lxml')
 
@@ -15,11 +16,17 @@ def remove_all(listobj):
     for val in temp:
         if str(val) == '':
             listobj.remove(val)
-    for j in range(4):
+    if "(Source:" in listobj:
+        listobj.pop()
+        listobj.pop()
+    elif "[Written" in listobj:
+        listobj.pop()
+        listobj.pop()
+        listobj.pop()
         listobj.pop()
     return listobj
 
-print(re.split(r'\s', summary))
+# print(re.split(r'\s', summary))
 # print(remove_all(re.split(r'\s', summary)))
 # print(summary)
 # print(" ".join(remove_all(re.split(r'\s',summary))))
@@ -34,17 +41,183 @@ print(re.split(r'\s', summary))
 # genre = match2.text
 # print(genre)
 
+genre_1 = []
+genre_2 = []
+genre_3 = []
+genre_4 = []
+genre_5 = []
+genre_6 = []
+genre_7 = []
+genre_8 = []
+
+def add_genres(gnrs):
+    genre_1.append(gnrs[0])
+
+    if len(gnrs) == 2:
+        genre_2.append(gnrs[1])
+        genre_3.append(None)
+        genre_4.append(None)
+        genre_5.append(None)
+        genre_6.append(None)
+        genre_7.append(None)
+        genre_8.append(None)
+
+    if len(gnrs) == 3:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(None)
+        genre_5.append(None)
+        genre_6.append(None)
+        genre_7.append(None)
+        genre_8.append(None)
+    
+    if len(gnrs) == 4:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(gnrs[3])
+        genre_5.append(None)
+        genre_6.append(None)
+        genre_7.append(None)
+        genre_8.append(None)
+
+    if len(gnrs) == 5:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(gnrs[3])
+        genre_5.append(gnrs[4])
+        genre_6.append(None)
+        genre_7.append(None)
+        genre_8.append(None)
+
+    if len(gnrs) == 6:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(gnrs[3])
+        genre_5.append(gnrs[4])
+        genre_6.append(gnrs[5])
+        genre_7.append(None)
+        genre_8.append(None)
+
+    if len(gnrs) == 7:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(gnrs[3])
+        genre_5.append(gnrs[4])
+        genre_6.append(gnrs[5])
+        genre_7.append(gnrs[6])
+        genre_8.append(None)
+
+    if len(gnrs) == 8:
+        genre_2.append(gnrs[1])
+        genre_3.append(gnrs[2])
+        genre_4.append(gnrs[3])
+        genre_5.append(gnrs[4])
+        genre_6.append(gnrs[5])
+        genre_7.append(gnrs[6])
+        genre_8.append(gnrs[7])
+
+
+
 """ genres = []
 gnrs = []
 for gnr in soup.find_all('span', itemprop = "genre"):
-    genre = gnr.text
-    gnrs.append(genre)
+    gnrs.append(gnr.text)
     
-print(gnrs)
+add_genres(gnrs)
+print(genre_1, genre_4, genre_8)
+gnrs.clear()
+print(genre_1, genre_4, genre_8) """
+
+""" print(gnrs)
 gnrs_cp = gnrs[:]
 genres.append(gnrs_cp)
 print(genres)
 gnrs.clear()
 print(gnrs)
 print(genres) """
-    # print(genre)
+
+
+def convtomins(hr, minute):
+    mins = 0
+    if hr == 'hr':
+        mins += int(hr)*60 
+    if minute == 'min':
+        mins += int(minute)
+    return mins
+
+test_source = requests.get("https://myanimelist.net/anime/33050/Fate_stay_night_Movie__Heavens_Feel_-_III_Spring_Song").text
+testsoup = bs(test_source, 'lxml')
+
+# for item in soup.find_all('td', class_ = 'borderClass'):
+#     if re.search(r"(Spring|Summer|Fall|Winter).\d{4}", item.text) != None:
+#         print(re.search(r"(Spring|Summer|Fall|Winter).\d{4}", item.text).group(1))
+
+
+# for item in soup.find_all('td', class_ = 'borderClass'):
+#     eplen = re.search(r"(\d{1,2})\s(\w+)\.\s?([a-z0-9]+)\s?(\w+)\.?", item.text)
+#     if eplen != None:
+#         print(eplen)
+#         # print(convtomins(eplen.group(2), eplen.group(4)))
+
+# for item in soup.find_all('td', class_ = 'borderClass'):
+#     if re.search(r"(R|PG)\s?[0-9+-]+\s?([0-9+]+)?", item.text) != None:
+#         print(re.search(r"(R|PG)\s?[0-9+-]+\s?([0-9+]+)?", item.text).group(0))
+
+# for item in soup.find_all('td', class_ = 'borderClass'):
+#     if re.search(r"(Manga|Visual novel|Game|Original)\s{2}", item.text) != None:
+#         print(re.search(r"(Manga|Visual novel|Game|Original)\s{2}", item.text).group(1))
+
+seasons = []
+ep_lens = []
+age_ratings = []
+materials = []
+
+foundseason = False
+foundeplen = False
+foundagerat = False
+foundmaterial = False
+for item in testsoup.find_all('td', class_ = 'borderClass'):
+    season = re.search(r"(Spring|Summer|Fall|Winter).\d{4}", item.text)
+    ep_len = re.search(r"(\d{1,2})\s(\w+)\.\s?([a-z0-9]+)\s?(\w+)\.?", item.text)
+    age_rating = re.search(r"(R|PG)\s?[0-9+-]+\s?([0-9+]+)?", item.text)
+    material = re.search(r"(Manga|Visual novel|Game|Original)\s{2}", item.text)
+
+    if season != None:
+        seasons.append(season.group(1))
+        foundseason = True
+
+    if ep_len != None:
+        ep_lens.append(ep_len.group(0))
+        foundeplen = True
+        
+    if age_rating != None:
+        age_ratings.append(age_rating.group(0))
+        foundagerat = True
+
+    if material != None:
+        materials.append(material.group(1))
+        foundmaterial = True
+
+if foundseason != True:
+    seasons.append(None)
+if foundeplen != True:
+    ep_lens.append(None)
+if foundagerat != True:
+    age_ratings.append(None)
+if foundmaterial != True:
+    materials.append(None)
+
+print(seasons, "\n", ep_lens, "\n", age_ratings, "\n", materials)
+
+
+""" tree = html.fromstring(source.content)
+season = tree.xpath('/html/body/div[1]/div/div[3]/div[2]/table/tbody/tr/td[1]/div/div[13]/a.text')
+print(season) """
+
+
+""" from lxml import etree
+
+newsoup = bs(source, 'html.parser')
+dom = etree.HTML(str(newsoup))
+print(dom.xpath('/html/body/div[1]/div/div[3]/div[2]/table/tbody/tr/td[1]/div/div[13]/a'))
+ """
